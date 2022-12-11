9b8ae891c4833a935e619081c6eac2ca169c90b8
123ihuayhua <88172318+123ihuayhua@users.noreply.github.com>
2022-12-11 11:26:44 -0500

    fin
    clase videojuego Soldado no se usa

---
+++ laboratorio18/Main.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package laboratorio18;
+
+import static laboratorio18.Menu.Menu;
+
+/**
+ *
+ * @author asus
+ */
+public class Main {
+    
+    public static void main (String [] args){
+        Menu();
+    }
+}
